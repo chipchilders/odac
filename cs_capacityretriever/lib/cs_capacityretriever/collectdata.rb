e91b50fe9c1e9eedaa5e33d1e386600bf5193fe9
Chip Childers <chipchilders@apache.org>
2013-08-30 14:16:19 -0400

    changing data storage format

--- cs_capacityretriever/lib/cs_capacityretriever/collectdata.rb
+++ cs_capacityretriever/lib/cs_capacityretriever/collectdata.rb
@@ -7,11 +7,71 @@ require 'json'
 module CsCapacityretriever
   class CollectData
     def collect()
-      client = CloudstackRubyClient::Client.new(Config::CLOUDSTACK_URL, Config::CLOUDSTACK_API_KEY, Config::CLOUDSTACK_SECRET_KEY, false)
-      zonedata = client.list_zones({"showcapacities" => "true"})
-      zonedata["collected_time"] = Time.new
+      collected_time = Time.new
       db = DbAccess.new()
-      db.write("zones", zonedata.to_json)
+      client = CloudstackRubyClient::Client.new(Config::CLOUDSTACK_URL, Config::CLOUDSTACK_API_KEY, Config::CLOUDSTACK_SECRET_KEY, false)
+
+      zones = client.list_zones({"showcapacities" => "true"})
+      zones["zone"].each do |zone|
+        zone["collected_time"] = collected_time
+        zone["zoneid"] = zone["id"]
+        db.write("zone", zone.to_json)
+
+        pods = client.list_pods({
+          "showcapacities" => "true", 
+          "pagesize" => 1000,
+          "page" => 1, 
+          "zoneid" => zone["id"]
+        })
+
+        pods["pod"].each do |pod|
+          pod["collected_time"] = collected_time
+          db.write("pod", pod.to_json)
+
+          clusters = client.list_clusters({
+            "showcapacities" => "true",
+            "pagesize" => 1000,
+            "page" => 1,
+            "podid" => pod["id"]
+          })
+
+          clusters["cluster"].each do |cluster|
+            cluster["collected_time"] = collected_time
+            db.write("cluster", cluster.to_json)
+
+            hosts = client.list_hosts({
+              "showcapacities" => "true",
+              "pagesize" => 1000,
+              "page" => 1,
+              "clusterid" => cluster["id"]
+            })
+
+            hosts["host"].each do |host|
+              host["collected_time"] = collected_time
+              db.write("host", host.to_json)
+            end
+
+            storagepools = client.list_storage_pools({
+              "pagesize" => 1000,
+              "page" => 1,
+              "clusterid" => cluster["id"]
+            })
+
+            storagepools["storagepool"].each do |storagepool|
+              storagepool["collected_time"] = collected_time
+              db.write("storagepool", storagepool.to_json)
+            end
+
+          end
+
+          regions = client.list_regions()
+          regions["region"].each do |region|
+            region["collected_time"] = collected_time
+            db.write("region", region.to_json)
+          end
+
+        end
+      end
     end
   end
 end
--- cs_capacityretriever/lib/cs_capacityretriever/dbaccess.rb
+++ cs_capacityretriever/lib/cs_capacityretriever/dbaccess.rb
@@ -9,10 +9,23 @@ module CsCapacityretriever
   class DbAccess
     def write(collection, document)
       db = MongoClient.new(Config::MONGO_HOST, Config::MONGO_PORT).db(Config::MONGO_DB)
-      coll = db.collection(collection)
       doc = JSON.parse(document)
+
+      coll = db.collection(collection + "_histories")
+      if doc.has_key?("id")
+        doc[collection + "id"] = doc["id"]
+      end
       id = coll.insert(doc)
-      puts id
+      puts "Write " + id.to_s + " to " + collection + "_histories"
+
+      coll = db.collection(collection + "s")
+      if doc.has_key?("id")
+        doc["_id"] = doc["id"]
+        doc.delete("id")
+      end
+      id = coll.update({ :_id => doc["_id"] }, doc, { :upsert => true })
+      puts "Wrote " + id.to_s + " to " + collection + "s"
+      return id
     end
   end
 end
